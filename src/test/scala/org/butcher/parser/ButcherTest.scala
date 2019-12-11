c4afe859b74fc03c0d149c127f2c37826140fb25
Keshav Murthy <keshav.murthy@latitudefinancial.com>
2019-12-12 06:58:24 +1100

    Add random to test

--- src/test/scala/org/butcher/parser/ButcherTest.scala
+++ src/test/scala/org/butcher/parser/ButcherTest.scala
@@ -5,13 +5,17 @@ import Butcher._
 import fastparse.Parsed
 import org.scalatest.prop.PropertyChecks
 
+import scala.util.Random
+
 class ButcherTest extends PropSpec with PropertyChecks with Matchers {
   property("token parsing - success") {
+    val rand = Random.alphanumeric.take(16).mkString
     val tokens =
       Table(
         ("input", "result"),
         ("first_name", Parsed.Success("first_name", 10)),
         ("last-name", Parsed.Success("last-name", 9)),
+        (rand, Parsed.Success(rand, 16)),
       )
 
     forAll(tokens) { (i: String, expected: Parsed.Success[String]) =>
