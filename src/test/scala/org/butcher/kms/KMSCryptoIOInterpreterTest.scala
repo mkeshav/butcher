94029720f25eade3ff685cd8777f5ba0bc0afd96
Keshav Murthy <keshav.murthy@latitudefinancial.com>
2019-12-16 09:44:06 +1100

    better tests

--- src/test/scala/org/butcher/kms/KMSCryptoIOInterpreterTest.scala
+++ src/test/scala/org/butcher/kms/KMSCryptoIOInterpreterTest.scala
@@ -2,18 +2,20 @@ package org.butcher.kms
 
 import java.nio.ByteBuffer
 
+import cats.data.EitherT
 import com.amazonaws.services.kms.AWSKMS
-import com.amazonaws.services.kms.model.{DecryptResult, GenerateDataKeyRequest, GenerateDataKeyResult}
+import com.amazonaws.services.kms.model.{DecryptResult, GenerateDataKeyResult}
 import com.amazonaws.util.Base64
+import org.butcher.kms.CryptoDsl.KMSCryptoIOInterpreter
+import org.butcher.kms.KMSService.parseAndDecrypt
 import org.scalamock.scalatest.MockFactory
 import org.scalatest.{FunSuite, Matchers}
-import KMSService._
 
-class KMSServiceTest extends FunSuite with MockFactory with Matchers {
+class KMSCryptoIOInterpreterTest extends FunSuite with Matchers with MockFactory{
   val b64EncodedPlainTextKey = "acZLXO+SWyeV95LYvUMExQtGeDHExNkAjvXbpbUEMK0="
   val b64EncodedCipherTextBlob = "AQIDAHhoNt+QMcK2fLVptebsdn939rqRYSkfDPtL70lK0fvadAGctDSWR9FFQo/sjJINvabqAAAAfjB8BgkqhkiG9w0BBwagbzBtAgEAMGgGCSqGSIb3DQEHATAeBglghkgBZQMEAS4wEQQMRXCvv+D0JW3bZA6hAgEQgDvx1mHmiC1xdu4IDLY38QmgcVJf3vxxrM/v5I9OFL/kls9DkP1fhZI1GJtiJ3nQaEsYjO5oBSmsRdNEpA=="
 
-  test("encrypt") {
+  test("kms io interpreter") {
     val kms = stub[AWSKMS]
     val generateDataKeyResult = new GenerateDataKeyResult()
       .withPlaintext(ByteBuffer.wrap(Base64.decode(b64EncodedPlainTextKey)))
@@ -23,26 +25,19 @@ class KMSServiceTest extends FunSuite with MockFactory with Matchers {
     val decryptResult = new DecryptResult().withPlaintext(ByteBuffer.wrap(Base64.decode(b64EncodedPlainTextKey)))
     (kms.decrypt _).when(*).returns(decryptResult)
 
+    val interpreter = new KMSCryptoIOInterpreter(kms)
     val f = for {
-      dk <- generateDataKey("foo").run(kms)
-      ed <- encryptWith("foo", dk).run(kms)
-    } yield ed
+      dk <- EitherT(interpreter.generateKey("foo"))
+      ed <- EitherT(interpreter.encrypt("foo", dk))
+      dec <- EitherT(interpreter.decrypt(ed))
+    } yield (ed, dec)
 
-    f.fold({t => println(t); false should be(true)}, {
+    f.value.unsafeRunSync().fold({t => println(t); false should be(true)}, {
       v =>
-        v should be("key:AQIDAHhoNt+QMcK2fLVptebsdn939rqRYSkfDPtL70lK0fvadAGctDSWR9FFQo/sjJINvabqAAAAfjB8BgkqhkiG9w0BBwagbzBtAgEAMGgGCSqGSIb3DQEHATAeBglghkgBZQMEAS4wEQQMRXCvv+D0JW3bZA6hAgEQgDvx1mHmiC1xdu4IDLY38QmgcVJf3vxxrM/v5I9OFL/kls9DkP1fhZI1GJtiJ3nQaEsYjO5oBSmsRdNEpA==,data:5gVr+Ca1Tqs9BirpPopOmw==")
-        parseAndDecrypt(v).run(kms) should be(Right("foo"))
+        v._1 should be("key:AQIDAHhoNt+QMcK2fLVptebsdn939rqRYSkfDPtL70lK0fvadAGctDSWR9FFQo/sjJINvabqAAAAfjB8BgkqhkiG9w0BBwagbzBtAgEAMGgGCSqGSIb3DQEHATAeBglghkgBZQMEAS4wEQQMRXCvv+D0JW3bZA6hAgEQgDvx1mHmiC1xdu4IDLY38QmgcVJf3vxxrM/v5I9OFL/kls9DkP1fhZI1GJtiJ3nQaEsYjO5oBSmsRdNEpA==,data:5gVr+Ca1Tqs9BirpPopOmw==")
+        v._2 should be("foo")
     })
-  }
 
-  test("encrypt exception") {
-    val kms = stub[AWSKMS]
-    (kms.generateDataKey _).when(_:GenerateDataKeyRequest).throwing(new Throwable("Say permission error"))
-    val f = for {
-      dk <- generateDataKey("foo").run(kms)
-      ed <- encryptWith("foo", dk).run(kms)
-    } yield ed
-    f.isLeft should be(true)
   }
 
 }
