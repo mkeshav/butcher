b46eb5607f4334b1f95ea961dc7df1fea5b7c505
Keshav Murthy <mkeshav@gmail.com>
2019-12-18 14:56:06 +1100

    Fix access of parsers

--- src/main/scala/org/butcher/parser/ButcherParser.scala
+++ src/main/scala/org/butcher/parser/ButcherParser.scala
@@ -20,21 +20,21 @@ final case class ColumnNamesEncryptExpr(override val columns: Seq[String], keyId
 final case class ColumnIndicesEncryptExpr(override val columns: Seq[Int], keyId: String) extends Expr with ColumnIndexExpr
 
 object ButcherParser {
-  def Newline[_: P] = P( NoTrace(StringIn("\r\n", "\n")) )
+  private def Newline[_: P] = P( NoTrace(StringIn("\r\n", "\n")) )
   private def numberParser[_: P]: P[Int] = P( CharIn("0-9").rep(1).!.map(_.toInt) )
   private def commaSeparatedIndicesParser[_: P]: P[Seq[Int]] = P(numberParser.!.map(_.toInt).rep(1, sep=","))
   private def tokenParser[_: P]: P[String] = P( CharIn("A-Za-z0-9_\\-").rep(1).!.map(_.mkString) )
   private def commaSeparatedTokensParser[_: P]: P[Seq[String]] = P(tokenParser.!.rep(min = 1, sep = ","))
-  def columnNamesLineMaskParser[_: P]: P[Expr]  = P(IgnoreCase("column names in [") ~ commaSeparatedTokensParser ~ IgnoreCase("] then mask") ~ Newline.rep(1).?).map {
+  private def columnNamesLineMaskParser[_: P]: P[Expr]  = P(IgnoreCase("column names in [") ~ commaSeparatedTokensParser ~ IgnoreCase("] then mask") ~ Newline.rep(1).?).map {
     ColumnNamesMaskExpr(_)
   }
-  def columnNamesLineEncryptParser[_: P]: P[Expr]  = P(IgnoreCase("column names in [") ~ commaSeparatedTokensParser ~ IgnoreCase("] then encrypt using kms key ") ~ tokenParser ~ Newline.rep(1).?).map {
+  private def columnNamesLineEncryptParser[_: P]: P[Expr]  = P(IgnoreCase("column names in [") ~ commaSeparatedTokensParser ~ IgnoreCase("] then encrypt using kms key ") ~ tokenParser ~ Newline.rep(1).?).map {
     case (columns, k) => ColumnNamesEncryptExpr(columns, k)
   }
-  def columnIndicesLineMaskParser[_: P]: P[Expr] = P(IgnoreCase("column indices in [") ~ commaSeparatedIndicesParser ~ IgnoreCase("] then mask") ~ Newline.rep(1).?).map {
+  private def columnIndicesLineMaskParser[_: P]: P[Expr] = P(IgnoreCase("column indices in [") ~ commaSeparatedIndicesParser ~ IgnoreCase("] then mask") ~ Newline.rep(1).?).map {
     ColumnIndicesMaskExpr(_)
   }
-  def columnIndicesLineEncryptParser[_: P]: P[Expr]  = P(IgnoreCase("column indices in [") ~ commaSeparatedIndicesParser ~ IgnoreCase("] then encrypt using kms key ") ~ tokenParser ~ Newline.rep(1).?).map {
+  private def columnIndicesLineEncryptParser[_: P]: P[Expr]  = P(IgnoreCase("column indices in [") ~ commaSeparatedIndicesParser ~ IgnoreCase("] then encrypt using kms key ") ~ tokenParser ~ Newline.rep(1).?).map {
     case (columns, k) => ColumnIndicesEncryptExpr(columns, k)
   }
 
--- src/test/scala/org/butcher/parser/ButcherParserTest.scala
+++ src/test/scala/org/butcher/parser/ButcherParserTest.scala
@@ -7,40 +7,6 @@ import org.scalatest.prop.PropertyChecks
 import scala.util.Random
 
 class ButcherParserTest extends PropSpec with PropertyChecks with Matchers {
-  property("indices parse - success") {
-    val l = (1 to 10).map(i => Random.nextInt(i*1000))
-    val data =
-      Table(
-        ("input", "result"),
-        (s"column indices in [${l.mkString(",")}] then mask", ColumnIndicesMaskExpr(l)),
-      )
-
-    forAll(data) { (i: String, expected: Expr) =>
-      val r = fastparse.parse(i, columnIndicesLineMaskParser(_))
-      r.fold(
-        onFailure = {(_, _, _) => false should be(true)},
-        onSuccess = {case (expr, _) => expr should be(expected)}
-      )
-    }
-  }
-
-  property("names parse - success") {
-    val l = (1 to 10).map(i => Random.alphanumeric.take(16).mkString)
-    val data =
-      Table(
-        ("input", "result"),
-        (s"column names in [ ${l.mkString(", ")} ] then mask", ColumnNamesMaskExpr(l)),
-      )
-
-    forAll(data) { (i: String, expected: Expr) =>
-      val r = fastparse.parse(i, columnNamesLineMaskParser(_))
-      r.fold(
-        onFailure = {(_, _, extra) => println(extra.trace().longMsg);false should be(true)},
-        onSuccess = {case (expr, _) => expr should be(expected)}
-      )
-    }
-  }
-
   property("multiline - names") {
     val lc1 = (1 to 10).map(_ => Random.alphanumeric.take(16).mkString)
     val lc2 = (1 to 10).map(_ => Random.alphanumeric.take(16).mkString)
