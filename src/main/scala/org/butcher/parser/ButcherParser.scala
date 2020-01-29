aaf9bcbe4342320400181fa98413a715e36445cd
Keshav Murthy <mkeshav@gmail.com>
2020-01-29 12:10:49 +1100

    Rename newline

--- src/main/scala/org/butcher/parser/ButcherParser.scala
+++ src/main/scala/org/butcher/parser/ButcherParser.scala
@@ -12,14 +12,14 @@ final case class UnknownExpr() extends Expr
 final case class EncryptColumnsWithPKExpression(encryptColumns: Seq[String], keyId: String, pkColumns: Seq[String]) extends Expr
 
 object ButcherParser {
-  private def Newline[_: P] = P( NoTrace(StringIn("\r\n", "\n")) )
+  private def newline[_: P] = P( NoTrace(StringIn("\r\n", "\n")) )
   private def tokenParser[_: P]: P[String] = P( CharIn("A-Za-z0-9_\\-").rep(1).!.map(_.mkString) )
   private def commaSeparatedTokensParser[_: P]: P[Seq[String]] = P(tokenParser.!.rep(min = 1, sep = ","))
 
   private def encryptUsingLine[_: P]  = P(IgnoreCase("encrypt columns [") ~ commaSeparatedTokensParser ~ IgnoreCase("] using key") ~ tokenParser)
   private def pkLine[_: P]  = P(IgnoreCase("with primary key columns [") ~ commaSeparatedTokensParser ~ IgnoreCase("]"))
 
-  def block[_: P]: P[EncryptColumnsWithPKExpression] = P(encryptUsingLine ~ Newline.rep(1).? ~ pkLine ).map {
+  def block[_: P]: P[EncryptColumnsWithPKExpression] = P(encryptUsingLine ~ newline.rep(1).? ~ pkLine ).map {
     case (encryptColumns, keyId, pkColumns) => EncryptColumnsWithPKExpression(encryptColumns, keyId, pkColumns)
   }
 }
